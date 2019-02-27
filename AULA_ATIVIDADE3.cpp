7f1c1dcdbd21cba6897b4ee444f0e97fe5030982
DEIVIDALUNO <47799917+DEIVIDALUNO@users.noreply.github.com>
2019-02-26 22:04:58 -0300

    Add files via upload

---
+++ AULA_ATIVIDADE3.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+
+//programa'le e imprime um valor'
+
+int main() 
+
+{
+
+
+
+   system ("cls");
+   
+     int A=0;
+     int B=0;
+   
+    printf("imforme um valor inteiro A: \n");
+    scanf("%i",&A);
+    printf("imforme um valor inteiro B :\n");
+    scanf("%i",&B);
+    printf("\n\n"); 
+       
+        
+ if(A>B)
+ {
+ 	printf ("A e maior que B!\n");
+ }
+   
+   else
+   {
+	  
+   if (A<B)
+  
+	printf("B e maior que A!\n");
+    }  	
+       
+   	   	
+
+    {	
+   printf("B e igual a A! \n");	   	
+    }            
+
+
+   //system ("pause");
+}
+   
+  
+   
+   
+ 
+   
+     
+
